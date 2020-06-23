589547e06c25d9b70b5c8839114e158c7374c957
Giang Vo <hoang.giang063@gmail.com>
2020-06-24 06:17:25 +0700

    add fragment

--- app/src/main/java/com/architecture/cleanmvvm/core/presenter/BasePresenter.kt
+++ app/src/main/java/com/architecture/cleanmvvm/core/presenter/BasePresenter.kt
@@ -1,6 +1,8 @@
 package com.architecture.cleanmvvm.core.presenter
 
-interface BasePresenter {
+import com.architecture.cleanmvvm.core.view.BaseView
+
+interface BasePresenter<T : BaseView> {
     fun onUpdateUi()
     fun onResume()
     fun onPause()
--- app/src/main/java/com/architecture/cleanmvvm/core/view/MainActivity.kt
+++ app/src/main/java/com/architecture/cleanmvvm/core/view/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.architecture.cleanmvvm.R
 import com.architecture.cleanmvvm.node1.demo.callback.ToDoCallBack
 import com.architecture.cleanmvvm.node1.demo.info.ToDoInfo
@@ -28,103 +30,111 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        hostFragment();
 
-        var toDoRepository: ToDoRepository =
-            RemoteToDoRepositoryImpl(RetrofitFactory.makeRetrofitService())
-        var todoUseCase: ToDoUseCase = ToDoUseCaseImpl(toDoRepository)
-
-        val database: AppDatabase = AppDatabase.getInstance(this);
-        var localRepository: ToDoRepository = LocalToDoRepositoryImpl(database.todoDao())
-
-
-
-        btnRemote.setOnClickListener {
-            val progress = ProgressDialog(this@MainActivity)
-            progress.setTitle("Loading")
-            progress.setMessage("Wait while loading...")
-            progress.setCancelable(false)
-            progress.show()
-            // load data by network (remote)
-            todoUseCase.buildUseCase(1)
-            todoUseCase(object : ToDoCallBack {
-                override fun onSuccess(expectedResult: ToDoInfo?) {
-                    Log.v("vhgiang", "=> remote solution run pass: " + expectedResult.toString())
-                    progress.dismiss()
-                }
-
-                override fun onFail(throwable: Throwable) {
-                    Log.v("vhgiang", "=> remote solution run fail: " + throwable?.message)
-                    progress.dismiss()
-                }
-
-            })
-        }
-
-        btnCache.setOnClickListener {
-            val progress = ProgressDialog(this@MainActivity)
-            progress.setTitle("Loading")
-            progress.setMessage("Wait while loading...")
-            progress.setCancelable(false)
-            progress.show()
-            // load data by network (remote)
-
-            var cacheRepository: ToDoRepository = CacheToDoRepositoryImpl(
-                true,
-                localRepository as LocalToDoRepositoryImpl,
-                toDoRepository as RemoteToDoRepositoryImpl
-            )
-
-            todoUseCase = ToDoUseCaseImpl(cacheRepository)
-            todoUseCase.buildUseCase(1)
-            todoUseCase(object : ToDoCallBack {
-                override fun onSuccess(expectedResult: ToDoInfo?) {
-                    Log.v("vhgiang", "=> cache solution run pass: " + expectedResult.toString())
-                    progress.dismiss()
-                }
-
-                override fun onFail(throwable: Throwable) {
-                    Log.v("vhgiang", "=> cache solution run fail: " + throwable?.message)
-                    progress.dismiss()
-                }
-
-            })
-        }
-
-        btnLocalDb.setOnClickListener {
-
-            val progress = ProgressDialog(this@MainActivity)
-            progress.setTitle("Loading")
-            progress.setMessage("Wait while loading...")
-            progress.setCancelable(false)
-            progress.show()
-            // load data by network (remote)
-
-
-            // load data by db
-            todoUseCase = ToDoUseCaseImpl(localRepository)
-            todoUseCase.buildUseCase(1)
-            todoUseCase(object : ToDoCallBack {
-                override fun onSuccess(expectedResult: ToDoInfo?) {
-                    Log.v("vhgiang", "=> db solution run pass: " + expectedResult.toString())
-
-                    progress.dismiss()
-                }
-
-                override fun onFail(throwable: Throwable) {
-                    Log.v("vhgiang", "=> db solution run fail: " + throwable?.message)
-
-                    progress.dismiss()
-                }
-
-            })
-
-        }
-        val intent: Intent = Intent()
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        setContentView(R.layout.activity_main)
+//
+//        var toDoRepository: ToDoRepository =
+//            RemoteToDoRepositoryImpl(RetrofitFactory.makeRetrofitService())
+//        var todoUseCase: ToDoUseCase = ToDoUseCaseImpl(toDoRepository)
+//
+//        val database: AppDatabase = AppDatabase.getInstance(this);
+//        var localRepository: ToDoRepository = LocalToDoRepositoryImpl(database.todoDao())
+//
+//
+//
+//        btnRemote.setOnClickListener {
+//            val progress = ProgressDialog(this@MainActivity)
+//            progress.setTitle("Loading")
+//            progress.setMessage("Wait while loading...")
+//            progress.setCancelable(false)
+//            progress.show()
+//            // load data by network (remote)
+//            todoUseCase.buildUseCase(1)
+//            todoUseCase(object : ToDoCallBack {
+//                override fun onSuccess(expectedResult: ToDoInfo?) {
+//                    Log.v("vhgiang", "=> remote solution run pass: " + expectedResult.toString())
+//                    progress.dismiss()
+//                }
+//
+//                override fun onFail(throwable: Throwable) {
+//                    Log.v("vhgiang", "=> remote solution run fail: " + throwable?.message)
+//                    progress.dismiss()
+//                }
+//
+//            })
+//        }
+//
+//        btnCache.setOnClickListener {
+//            val progress = ProgressDialog(this@MainActivity)
+//            progress.setTitle("Loading")
+//            progress.setMessage("Wait while loading...")
+//            progress.setCancelable(false)
+//            progress.show()
+//            // load data by network (remote)
+//
+//            var cacheRepository: ToDoRepository = CacheToDoRepositoryImpl(
+//                true,
+//                localRepository as LocalToDoRepositoryImpl,
+//                toDoRepository as RemoteToDoRepositoryImpl
+//            )
+//
+//            todoUseCase = ToDoUseCaseImpl(cacheRepository)
+//            todoUseCase.buildUseCase(1)
+//            todoUseCase(object : ToDoCallBack {
+//                override fun onSuccess(expectedResult: ToDoInfo?) {
+//                    Log.v("vhgiang", "=> cache solution run pass: " + expectedResult.toString())
+//                    progress.dismiss()
+//                }
+//
+//                override fun onFail(throwable: Throwable) {
+//                    Log.v("vhgiang", "=> cache solution run fail: " + throwable?.message)
+//                    progress.dismiss()
+//                }
+//
+//            })
+//        }
+//
+//        btnLocalDb.setOnClickListener {
+//
+//            val progress = ProgressDialog(this@MainActivity)
+//            progress.setTitle("Loading")
+//            progress.setMessage("Wait while loading...")
+//            progress.setCancelable(false)
+//            progress.show()
+//            // load data by network (remote)
+//
+//
+//            // load data by db
+//            todoUseCase = ToDoUseCaseImpl(localRepository)
+//            todoUseCase.buildUseCase(1)
+//            todoUseCase(object : ToDoCallBack {
+//                override fun onSuccess(expectedResult: ToDoInfo?) {
+//                    Log.v("vhgiang", "=> db solution run pass: " + expectedResult.toString())
+//
+//                    progress.dismiss()
+//                }
+//
+//                override fun onFail(throwable: Throwable) {
+//                    Log.v("vhgiang", "=> db solution run fail: " + throwable?.message)
+//
+//                    progress.dismiss()
+//                }
+//
+//            })
+//
+//        }
+//        val intent: Intent = Intent()
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
     }
 
-
+    private fun hostFragment(fragment: BaseFragment){
+        if(supportFragmentManager.findFragmentByTag(fragment.javaClass.simpleName)==null){
+            var fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container,fragment.getInstance(),fragment.javaClass.simpleName)
+            fragmentTransaction.commit()
+        }
+    }
 }
 
 object RetrofitFactory {
